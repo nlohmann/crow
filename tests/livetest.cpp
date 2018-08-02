615cb5fb403c92e5f6e9b7b075ad17061f733c2f
Niels Lohmann <mail@nlohmann.me>
2018-08-02 18:15:05 +0200

    :checkered_flag: more work on the windows version

--- tests/livetest.cpp
+++ tests/livetest.cpp
@@ -13,27 +13,21 @@ int main()
     {
         c.add_extra_context({{"job_number", job_number}});
         c.add_tags_context({{"ci", "appveyor"}});
+        const char* build_number = getenv("APPVEYOR_BUILD_NUMBER");
+        if (build_number)
+        {
+            c.add_extra_context({{"build_number", build_number}});
+        }
     }
     else
     {
         job_number = getenv("TRAVIS_JOB_NUMBER");
-        const char* build_number = getenv("APPVEYOR_BUILD_NUMBER");
         if (job_number)
         {
             c.add_extra_context({{"job_number", job_number}});
             c.add_tags_context({{"ci", "travis"}});
-            if (build_number)
-            {
-                c.add_extra_context({{"build_number", build_number}});
-            }
         }
     }
-
-    c.add_extra_context({{"NLOHMANN_CROW_CMAKE_SYSTEM_NAME", NLOHMANN_CROW_CMAKE_SYSTEM_NAME}});
-    c.add_extra_context({{"NLOHMANN_CROW_CMAKE_SYSTEM_VERSION", NLOHMANN_CROW_CMAKE_SYSTEM_VERSION}});
-    c.add_extra_context({{"NLOHMANN_CROW_OS_RELEASE", NLOHMANN_CROW_OS_RELEASE}});
-    c.add_extra_context({{"NLOHMANN_CROW_OS_VERSION", NLOHMANN_CROW_OS_VERSION}});
-    c.add_extra_context({{"NLOHMANN_CROW_VER", NLOHMANN_CROW_VER}});
 
     c.capture_message(NLOHMANN_CROW_CXX);
     std::cout << "sent message" << std::endl;
