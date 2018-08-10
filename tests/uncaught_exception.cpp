de14ec730bf0f4d4ad2201464572fc91b35e7b91
Niels Lohmann <mail@nlohmann.me>
2018-08-10 23:10:45 +0200

    :white_check_mark: added test case for exception handling

--- tests/uncaught_exception.cpp
+++ tests/uncaught_exception.cpp
@@ -4,10 +4,12 @@
 #include <thirdparty/curl_wrapper/curl_wrapper.hpp>
 
 using crow = nlohmann::crow;
+crow *c = nullptr;
 
 // a termination handler that checks the state of the messages sent to Sentry
 void my_termination_handler()
 {
+    delete c;
     std::lock_guard<std::mutex> lock(curl_wrapper::mutex);
     std::cout << "payload = " << std::setw(4) << curl_wrapper::results << std::endl;
     if (curl_wrapper::results[0]["payload"]["exception"][0]["value"] != "oops!")
@@ -26,7 +28,7 @@ int main()
     std::set_terminate(my_termination_handler);
 
     // define a client and install the handlers
-    crow c("http://abc:def@sentry.io/123", nullptr, 1.0, true);
+    c = new crow("http://abc:def@sentry.io/123", nullptr, 1.0, true);
 
     // throw an uncaught exception
     throw std::runtime_error("oops!");
