c92b10abb0cf098d7d6a911ac9e567edb82d6db5
Niels Lohmann <niels.lohmann@daimler.com>
2018-08-14 15:58:40 +0200

    :checkered_flag: another try

--- tests/uncaught_exception.cpp
+++ tests/uncaught_exception.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <exception>
 #include <crow/crow.hpp>
 
@@ -8,8 +9,16 @@ crow* client = nullptr;
 // a termination handler that checks the state of the messages sent to Sentry
 void my_termination_handler()
 {
+    std::cout << "entered my_termination_handler()" << std::endl;
+
     assert(client->m_posts > 0);
-    bool success = not client->get_last_event_id().empty();
+
+    std::cout << "client->m_posts = " << client->m_posts << std::endl;
+
+    std::string last_id = client->get_last_event_id();
+    std::cout << "last_id = " << last_id << std::endl;
+
+    bool success = not last_id.empty();
     delete client;
     std::exit(success ? 0 : 1);
 }
@@ -22,6 +31,8 @@ int main()
     // define a client and install the handlers
     client = new crow("http://abc:def@127.0.0.1:5000/1");
     client->install_handler();
+
+    std::cout << "installed handlers" << std::endl;
 
     // throw an uncaught exception
     throw std::runtime_error("oops!");
